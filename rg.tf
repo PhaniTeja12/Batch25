daaee18c3abcd21980b67dc942ca5ec327fc7197
PhaniTeja12 <135310709+PhaniTeja12@users.noreply.github.com>
2023-07-18 14:52:35 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "RG_Creation" {
   name = "Phani-rg"
   location = "eastus"
 }
+resource "azurerm_resource_gropu" "RG1_Creation" {
+name = "Teju-rg"
+location = "westus"
+}
